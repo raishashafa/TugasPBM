57df897b38fbe52cdea445c1583a6c9f21836877
raishashafa <4521210049@univpancasila.ac.id>
2024-04-01 13:38:43 +0700

    Praktikum 4

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('I Am Poor', 
+          style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+        ),
+    )
+  );
+}
